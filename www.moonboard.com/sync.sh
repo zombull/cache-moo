bca89455d74b35e5b27d3716caf31cbfcfa780e4
Sean Christopherson <sean.j.c@gmail.com>
2018-03-24 21:26:24 -0700

    moonboard: drop count to 190, add workaround for borked DB
    Moonboard is deleting junk problems (yay!) but fucked up their
    database in the process, e.g. some problems are indexed but no
    longer exist.

--- www.moonboard.com/sync.sh
+++ www.moonboard.com/sync.sh
@@ -2,13 +2,24 @@
 source cookie
 
 if [[ $1 == "problems" ]]; then
-    for i in {1..191}; do
+    for i in {1..190}; do
         if [[ ! -f "problems.$i.json" ]]; then
             printf "%s\n" "curl 'https://www.moonboard.com/Problems/GetProblems' -H '$cookie' -H 'Origin: https://www.moonboard.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: https://www.moonboard.com/Problems/Index' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data \$'sort=&page=$i&pageSize=100&group=&filter=setupId~eq~\'1\'' --compressed > problems.$i.json" | bash
             sleep .1
         fi
-
     done
+    # for i in {1891..1900}; do
+    #     if [[ ! -f "problems.$i.json" ]]; then
+    #         printf "%s\n" "curl 'https://www.moonboard.com/Problems/GetProblems' -H '$cookie' -H 'Origin: https://www.moonboard.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: https://www.moonboard.com/Problems/Index' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data \$'sort=&page=$i&pageSize=10&group=&filter=setupId~eq~\'1\'' --compressed > problems.$i.json" | bash
+    #         sleep .1
+    #     fi
+    # done
+    # for i in {18951..18960}; do
+    #     if [[ ! -f "problems.$i.json" ]]; then
+    #         printf "%s\n" "curl 'https://www.moonboard.com/Problems/GetProblems' -H '$cookie' -H 'Origin: https://www.moonboard.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: https://www.moonboard.com/Problems/Index' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data \$'sort=&page=$i&pageSize=1&group=&filter=setupId~eq~\'1\'' --compressed > problems.$i.json" | bash
+    #         sleep .1
+    #     fi
+    # done
 fi
 
 guid='6BE4A8C1-02C7-4BA4-9589-769ED0B3571C'
