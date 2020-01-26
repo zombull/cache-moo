43d69c8c49b60ce594221311f050722c81b99ead
Sean Christopherson <sean.j.c@gmail.com>
2020-01-26 13:10:52 -0800

    moonboard: 2017: sync problems 01/26/2020

--- www.moonboard.com/sync.sh
+++ www.moonboard.com/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source cookie
 
-for i in {1..197}; do
+for i in {1..1755}; do
     if [[ ! -f "problems.$i.json" ]]; then
-        printf "%s\n" "curl 'https://www.moonboard.com/Problems/GetProblems' -H '$cookie' -H 'Origin: https://www.moonboard.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: https://www.moonboard.com/Problems/Index' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data $'sort=&page=$i&pageSize=100&group=&filter=Configuration~eq~\'40%C2%B0+MoonBoard\'' --compressed > problems.$i.json" | bash
+        printf "%s\n" "curl 'https://moonboard.com/Problems/GetProblems' -H 'Connection: keep-alive' -H 'Accept: */*' -H 'Origin: https://moonboard.com' -H 'X-Requested-With: XMLHttpRequest' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://moonboard.com/Problems/Index' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H '$cookie' --data $'sort=&page=$i&pageSize=15&group=&filter=Configuration~eq~\'40%C2%B0+MoonBoard\'~and~setupId~eq~\'15\'' --compressed > problems.$i.json" | bash
         sleep .1
     fi
 done
